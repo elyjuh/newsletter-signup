da7b4fcd9198572d815b22c562759a7d74644c5e
Elijah Gallosa <elaygallosa@gmail.com>
2025-08-10 23:09:34 +0800

    Modified the condition and made the email more stricter using regex

--- js/script.js
+++ js/script.js
@@ -2,21 +2,21 @@ const emailInput = document.getElementById("emailInput");
 const errorLabel = document.getElementById("errorLabel");
 const submitBtn = document.getElementById("submitBtn");
 
-submitBtn.addEventListener ("click",  (event) => {
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+submitBtn.addEventListener("click", (event) => {
     event.preventDefault();
 
     const email = emailInput.value.trim();
 
-    if (!email.includes("@")) {
+    if (!emailRegex.test(email)) {
         errorLabel.classList.add("open");
-        emailInput.classList.add("error");   
+        emailInput.classList.add("error");
     } else {
         errorLabel.classList.remove("open");
         emailInput.classList.remove("error");
 
         sessionStorage.setItem("userEmail", email);
         window.location.href = `success-page.html`;
-        
-    }   
+    }
 });
-
